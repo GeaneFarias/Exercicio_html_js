b4c4a8a055f3cc1d363fe736fdf5a06f2b03ae03
Geane Farias <fariassantos.geane@gmail.com>
2023-10-04 16:08:25 -0300

    g

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ const form = document.getElementById("formu")
 
 
 form.addEventListener("submit", function(e){
-    e.preventDefaul();
+    e.preventDefault();
 
     const valorum = document.getElementById("1num")
     const valordois = document.getElementById("2num")
-    const mensagemdevalid1 = `O valor <b>${valorum.value}</b> é menor que o valor <b>${valordois.value}</b>`
-    const mensagemdevalid2 = `O valor <b>${valorum.value}</b> é maior que o valor <b>${valordois.value}</b>`
+    const mensagemdevalid1 = `O valor <b>${valorum.value}</b> é menor que o valor de <b>${valordois.value}</b>`
+    const mensagemdevalid2 = `O valor <b>${valorum.value}</b> é maior que o valor de <b>${valordois.value}</b>`
 
     if (valorum.value < valordois.value){
         document.querySelector('.mensagedvalid').innerHTML = mensagemdevalid1
