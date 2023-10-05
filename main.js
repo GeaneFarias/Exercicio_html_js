7269ab2dd9e344ae04491d8c75e6b6cbfbd7b160
Geane Farias <fariassantos.geane@gmail.com>
2023-10-05 01:12:47 -0300

    Ajustes recomendados.

--- main.js
+++ main.js
@@ -5,15 +5,32 @@ const form = document.getElementById("formu")
 form.addEventListener("submit", function(e){
     e.preventDefault();
 
-    const valorum = document.getElementById("1num")
-    const valordois = document.getElementById("2num")
-    const mensagemdevalid1 = `O valor <b>${valorum.value}</b> é menor que o valor de <b>${valordois.value}</b>`
-    const mensagemdevalid2 = `O valor <b>${valorum.value}</b> é maior que o valor de <b>${valordois.value}</b>`
+    const valorum = parseFloat(document.getElementById("1num").value);
 
-    if (valorum.value < valordois.value){
-        document.querySelector('.mensagedvalid').innerHTML = mensagemdevalid1
-    }else{
-        document.querySelector('.mensagedvalid').innerHTML = mensagemdevalid2
+    const valordois = parseFloat(document.getElementById("2num").value);
+
+    const mensagemdevalid1 = `O valor <b>${valorum}</b> é menor que o valor  <b>${valordois}</b>`
+
+    const mensagemdevalid2 = `O valor <b>${valorum}</b> é maior que o valor  <b>${valordois}</b>`
+
+    const mensagemdevalid3 = `O valor <b>${valorum}</b> é igual a <b>${valordois}</b>`
+
+
+    if (valorum < valordois){
+        document.querySelector('.mensagedvalid').style.display = "block";
+
+        document.querySelector('.mensagedvalid').innerHTML = mensagemdevalid1;
+
+    }else if(valorum > valordois){
+        document.querySelector('.mensagedvalid').style.display = "block";
+
+        document.querySelector('.mensagedvalid').innerHTML = mensagemdevalid2;
+    }
+    else{
+        document.querySelector('.mensagedvalid').style.display = "block";
+        
+        document.querySelector('.mensagedvalid').innerHTML = mensagemdevalid3;
+
     }
 
 
